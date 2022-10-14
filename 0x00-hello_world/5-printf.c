69e0254f5d1ca8aae0f0c7e0f15a3d09c693fff6
harmonyordu <harmonyordu@gmail.com>
2022-10-14 16:48:43 -0700

    Trailing error fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
-#include <stdio.h>  
+#include <stdio.h>
 /**
  *main -is the begining point
- *Return: value should be zero 
+ *Return: value should be zero
  *
  *A program that uses printf to print a statement
- */  
+ */
 
 int main(void)
 
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-}     
+}
