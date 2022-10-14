a891f9830d1251463043077cbeedc39b7e725244
harmonyordu <harmonyordu@gmail.com>
2022-10-14 16:31:29 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  
+/**
+ *main -is the begining point
+ *Return: value should be zero 
+ *
+ *A program that uses printf to print a statement
+ */  
+
+int main(void)
+
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}     
