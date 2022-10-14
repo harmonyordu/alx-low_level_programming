547a8fc922cd7f195f0537025ba000bc6f391d75
harmonyordu <harmonyordu@gmail.com>
2022-10-14 16:07:24 -0700

    new commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+include < stdio.h >
+/**
+ *main -is the begining point
+ *Return: value should be zero
+ *
+ *A program that uses puts to print
+ */
+
+int main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
