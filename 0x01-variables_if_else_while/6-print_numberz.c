b48a7c8c1ab47d5833101c44be6f2531c70b79e5
harmonyordu <harmonyordu@gmail.com>
2022-10-14 21:50:25 -0700

    first commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - The program starts here
+ *Return: value is zero
+ */
+
+int main(void)
+{
+
+int x;
+for (x = 0; x < 10; x++)
+
+	putchar(x + '0');
+
+putchar('\n');
+return (0);
+
+}
