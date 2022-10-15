6705a617b37c9fd891bd51767eec47672a28fc87
harmonyordu <harmonyordu@gmail.com>
2022-10-14 20:16:51 -0700

    New commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - The program starts here
+ *Return: value is zero
+ *
+ */
+
+int main(void)
+{
+
+char x;
+for (x = 'a'; x <= 'z'; x++)
+putchar(x);
+putchar('\n');
+return (0);
