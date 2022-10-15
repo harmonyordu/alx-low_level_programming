e0dfbd183a066b1a329687eab9f1d945f12c0d44
harmonyordu <harmonyordu@gmail.com>
2022-10-14 21:05:07 -0700

    new commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - The program starts here
+ *Return: value is zero
+ */
+
+int main(void)
+
+{
+
+int x = 0;
+for (; x < 10; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
