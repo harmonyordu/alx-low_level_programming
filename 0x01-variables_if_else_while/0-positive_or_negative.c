cbb3b820006e95af2ebda1ac4cbd781d95151bc2
harmonyordu <harmonyordu@gmail.com>
2022-10-14 19:31:10 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,45 @@
+#include <stdio.>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ *main - Beginning of the program
+ *
+ *rand - Generates a random number
+ *
+ *Return: value is zero
+ *
+ */
+
+int main(void)
+{
+
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+
+{
+
+	printf("%d is positive\n", n);
+
+}
+
+if (n == 0)
+
+{
+
+	printf("%d is zero\n", n);
+}
+
+else
+
+{
+
+	printf("%d is negative\n", n);
+
+}
+
+return (0);
+
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+# This is a project for low level programming with C ALX Software Engineering program, This contains projects and task on conditionals and loops
