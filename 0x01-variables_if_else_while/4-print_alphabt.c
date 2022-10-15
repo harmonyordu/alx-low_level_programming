e57df16d25fa4ffc87249e6ce0a697870c2644fd
harmonyordu <harmonyordu@gmail.com>
2022-10-14 20:51:01 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - The program starts here
+ *Return: value is zero
+ */
+
+int main(void)
+{
+
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+	if (x == 'q' || a == 'e')
+{
+	continue;
+}
+putchar(x);
+}
+
+putchar('\n');
+return (0);
+}
