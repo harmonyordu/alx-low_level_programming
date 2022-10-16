65380268c62a0611c785257b0904c088a32ef85c
harmonyordu <harmonyordu@gmail.com>
2022-10-16 10:29:07 -0700

    changed to system function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 
-write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful - Dora Korpar, 2015-10-19"));
-
+system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-19 ");
 return (1);
 }
