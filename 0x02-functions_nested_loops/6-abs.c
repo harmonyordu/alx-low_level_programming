d6948496eefa6dab2f3eade31e3b3bd66c5022fd
harmonyordu <harmonyordu@gmail.com>
2022-10-19 15:54:35 -0700

    new commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs - The main function of the program
+ *@n: The variable to make absolute
+ *Return: int or int times a negative number
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+
+	else
+		return (n * -1);
+
+}
