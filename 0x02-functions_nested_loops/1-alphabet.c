c525d927b18914a16d9f109b2e4936dfc9dbde9d
harmonyordu <harmonyordu@gmail.com>
2022-10-19 11:44:16 -0700

    new function print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_alphabet-The program starts from this function
+ *Return- Always return zero
+ *Description- this program Write a function that prints the alphabet lowercase
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c < = 'z')
+
+	{
+		_putchar(c);
+
+		c++;
+	}
+
+return (0);
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_alphabet_x10- The program starts here
+ *Return- returns zero 0
+ *Description- a function that prints 10 times the alphabet, in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i = 0;
+
+	while (i < 9)
+	{
+		while (c <= z)
+		{
+			_putchar(c);
+			c++
+		}
+		i++;
+		_putchar('\n');
+	}
+	return (0);
+}
+
+
+
+
