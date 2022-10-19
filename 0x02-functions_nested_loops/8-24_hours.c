0ecfa582a03ab3c3e1a5ab11d22781e7e9bdb84b
harmonyordu <harmonyordu@gmail.com>
2022-10-19 16:49:28 -0700

    new commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  *jack_bauer -function that prints every minute of the day of Jack Bauer
  *Return:null
@@ -10,6 +11,8 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 23; i++)
 {
+
+		for (j = 0; j <= 59; j++)
 		{
 		_putchar(i / 10 + '0');
 		_putchar(i % 10 + '0');
