58faf637737047f03390b291041c4ea98fb589e2
harmonyordu <harmonyordu@gmail.com>
2022-10-19 10:33:03 -0700

    added main.h as header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,25 @@
-#include<stdio.h>
+#include "main.h"
 /**
  *main-Entry point
- *Return: Returns 1 (Success)
+ *Description- The program prints Putchar
+ *Return: The program returns 0 (Success)
  */
 
 int main(void)
 
 {
-	write(1, "_putchar\n", 10);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
+
 
 }
 
