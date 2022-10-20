279db91c375f2beefff5a0f027f9fff4cc7d6912
harmonyordu <harmonyordu@gmail.com>
2022-10-19 17:37:56 -0700

    new commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ *add - The function adds to value
+ *@a: The first variable to be added
+ *@b: The second number to be added
+ *Return: Integer value
+ */
+
+int add(int a, int b)
+
+{
+	return (a + b);
+
+}
