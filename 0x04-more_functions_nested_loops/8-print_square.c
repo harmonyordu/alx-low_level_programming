d32cc7e1d4e0bd8ba8580a07998ee7e1e051edcf
harmonyordu <harmonyordu@gmail.com>
2022-10-20 17:27:28 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+
+
+/**
+
+ * print_square - prints a square
+
+ * @size: size of square
+
+ * Return: nothing
+
+ */
+
+
+
+void print_square(int size)
+
+{
+
+	int x, y;
+
+
+
+	y = 0;
+
+
+
+	if (size < 1)
+
+		_putchar('');
+
+
+
+	while (y < size)
+
+	{
+
+		x = 0;
+
+		while (x < size)
+
+		{
+
+			_putchar('#');
+
+			x++;
+
+		}
+
+		_putchar('\n');
+
+		y++;
+
+	}
+
+}
