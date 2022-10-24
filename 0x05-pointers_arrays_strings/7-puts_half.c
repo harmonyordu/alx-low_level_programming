7aeb7e67ff5e980e5bda36bec15d9384a828645f
harmonyordu <harmonyordu@gmail.com>
2022-10-24 16:58:42 -0700

    corrections

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,18 +13,14 @@ void puts_half(char *str)
 {
 
 	int j = 0;
-
 	int k;
-
 
 	while (str[j] != '\0')
 
 	{
 
 		j++;
-
 	}
-
 
 
 	if (j % 2 == 1)
@@ -32,7 +28,6 @@ void puts_half(char *str)
 	{
 
 		k = (j - 1) / 2;
-
 		k += 1;
 
 	}
@@ -45,17 +40,12 @@ void puts_half(char *str)
 
 	}
 
-
-
 	for (; k < j; k++)
 
 	{
 
 		_putchar(str[k]);
-
 	}
-
-
 
 	_putchar('\n');
 
