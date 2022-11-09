fde8800ac01a049540749f3d500cc3658fe9dac1
harmonyordu <harmonyordu@gmail.com>
2022-11-09 12:15:47 -0800

    new commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+  * main - This prints the number of arguments passed
+  * @argc: argument counter
+  * @argv: argument vector
+  * Return: 0
+  */
+
+
+int main(int argc, char *argv[] __attribute__((unused)))
+
+{
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+
+}
