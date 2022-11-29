0c1cd8c18ee7c3ba6456f7c1e0ae9ea9b8f48185
harmonyordu <harmonyordu@gmail.com>
2022-11-29 09:49:15 -0800

    adding new files

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,103 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * createNode - creates new node
+ * @num: name
+ * Return: A new element
+ */
+
+listint_t *createNode(const int num)
+
+{
+
+	listint_t *new;
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+
+	{
+
+		return (NULL);
+
+	}
+
+	new->n = num;
+	new->next = NULL;
+
+	return (new);
+
+}
+
+/**
+ * getLastNode - get the last node
+ * @HeadNode: first node
+ * Return: last element
+ */
+
+listint_t *getLastNode(listint_t *HeadNode)
+{
+	if (!HeadNode)
+
+		return (NULL);
+
+	if (HeadNode->next == NULL)
+
+		return (HeadNode);
+	else
+
+		return (getLastNode(HeadNode->next));
+}
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: first element
+ * @n: number
+ * Return: new node
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+
+{
+
+	listint_t *new, *last;
+
+	if (head == NULL)
+
+		return (NULL);
+
+	new = createNode(n);
+
+	if (new == NULL)
+
+		return (NULL);
+
+	if (*head == NULL)
+
+	{
+
+		*head = new;
+
+	}
+
+	else
+
+	{
+
+		last = getLastNode(*head);
+
+		if (last == NULL)
+
+		{
+			printf("failed\n");
+			return (NULL);
+
+		}
+
+		last->next = new;
+	}
+
+	return (new);
+
+}
